2d694f717ec9d54024c0728cace9b71c4fbfd80f
tatiana duran <tatiiss309@gmail.com>
2023-10-04 22:36:50 -0500

    feat: :sparkles: Agregar relaciones Se generan las relaciones e importacione con los demas documentos de la bd de la cual se relaciona libro

--- storage/libro.js
+++ storage/libro.js
@@ -1,61 +1,163 @@
-import {env} from "../config.js"
-const uri = `${env.ssl + env.hostName}:${env.port}/`;
+import { getOne as getOneCategoria  }  from "./categoria.js";
+import { getOne as getOneAutor  }  from "./autor.js";
+import {env} from "../config.js";
 
-//testear si la uri esta bien programacion defenciv
-// console.log(uri)
+const uri = `${env.ssl + env.hostName}:${env.port}`;
+const config = {method: undefined, headers: {"Content-Type": "application/json"}};
 
-const config = {method:null,headers:{"content-type": "application/json"}};
-
-export const getAll =async() =>{
-    config.method= "GET";
-    let res = await(await fetch(`${uri}/libro`,config)).json();
-    console.log(res)
-}
-
-export const post =async(obj) =>{
-    config.method= "POST";
-    config.body = JSON.stringify(obj);
-    let res = await(await fetch(`${uri}/libro`,config)).json();
-    return res; 
-}
-
-export const deleteOne =async(id) =>{
-    if(typeof id !== "number") {
-        return{status:400, message:`Los datos '${id}' no cumple con el formato `}}
-    config.method= "DELETE";
-    let res = await(await fetch(`${uri}/libro/${id}`, config)).json();
-    return res; 
-}
-
-export const putOne =async(obj={}) =>{
-    if(!obj.id){return {status:400, message:`Usuario introduzca los datos `}}
-    // const {id, autorId, categoriaId, editorialId, titulo, fechaLanzamiento, isbn, numPaginacion, estadoId} = obj;
-    const {fechaLanzamiento} = obj;
+const validarExtructura = (data={})=>{
+    if(data.constructor.name !== "Object" || Object.keys(data).length==0) return {status: 400, message: `Usuario envie los datos`};
+    const {
+        autorId=null, 
+        categoriaId=null, 
+        editorialId=null, 
+        titulo=null, 
+        fechaLanzamiento=null, 
+        isbn=null, 
+        numPaginacion=null, 
+        estadoId=null
+    } = data;
     let date = new Date(fechaLanzamiento);
-    if(!(date && date.getFullYear()<=2040)) {
-        return{status:400, message:`Los datos'${fechaLanzamiento}' no cumple con el formato`}}
-    // if(typeof id !== 'number') {
-    //     return{status:400, message:`Los datos'${id}' no cumple con el formato`}}
-    // if(typeof autorId !== 'number') {
-    //     return{status:400, message:`Los datos'${autorId}' no cumple con el formato`}}
-    // if(typeof categoriaId !== 'number') {
-    //     return{status:400, message:`Los datos'${categoriaId}' no cumple con el formato`}} 
-    // if(typeof editorialId !== 'number') {
-    //    return{status:400, message:`Los datos'${editorialId}' no cumple con el formato`}}
-    // if(typeof titulo !== 'number') {
-    //     return{status:400, message:`Los datos'${titulo}' no cumple con el formato`}}
-    // if(typeof isbn !== 'string') {
-    //     return{status:400, message:`Los datos'${isbn}' no cumple con el formato`}}
-    // if(typeof numPaginacion !== 'number') {
-    //     return{status:400, message:`Los datos'${numPaginacion}' no cumple con el formato`}}
-    // if(typeof estadoId !== 'number') {
-    //     return{status:400, message:`Los datos'${estadoId}' no cumple con el formato`}} 
-    return "todo ok"
-};
+    if(!(date && date.getFullYear()<=2040)) return {status: 400, message: `La fechaLanzamiento '${fechaLanzamiento}' no cumple con el formato`};
+    
+    if(typeof autorId !== 'number') return {status: 400, message: `El autorId '${autorId}' no cumple con el formato`};
+    if(typeof categoriaId !== 'number') return {status: 400, message: `El categoriaId '${categoriaId}' no cumple con el formato`};
+    if(typeof editorialId !== 'number') return {status: 400, message: `El editorialId '${editorialId}' no cumple con el formato`};
+    if(typeof titulo !== 'string') return {status: 400, message: `El titulo '${titulo}' no cumple con el formato`};
+    if(typeof isbn !== 'string') return {status: 400, message: `El isbn '${isbn}' no cumple con el formato`};
+    if(typeof numPaginacion !== 'number') return {status: 400, message: `El numPaginacion '${numPaginacion}' no cumple con el formato`};
+    if(typeof estadoId !== 'number') return {status: 400, message: `El estadoId '${estadoId}' no cumple con el formato`};
+    return data;
+}
+const validarDataBasic = (data={})=>{
+    if(data.constructor.name !== "Object" || Object.keys(data).length==0) return {status: 400, message: `Usuario envie los datos`};
+    const {
+        titulo=null, 
+        fechaLanzamiento=null, 
+        isbn=null, 
+        numPaginacion=null, 
+    } = data;
+    let date = new Date(fechaLanzamiento);
+    if(!(date && date.getFullYear()<=2040)) return {status: 400, message: `La fechaLanzamiento '${fechaLanzamiento}' no cumple con el formato`};
+    if(typeof titulo !== 'string') return {status: 400, message: `El titulo '${titulo}' no cumple con el formato`};
+    if(typeof isbn !== 'string') return {status: 400, message: `El isbn '${isbn}' no cumple con el formato`};
+    if(typeof numPaginacion !== 'number') return {status: 400, message: `El numPaginacion '${numPaginacion}' no cumple con el formato`};
+    return data;
+}
+export const getAll = async()=>{
+    config.method = "GET";
+    // config.body = "";
+    let res = await (await fetch(`${uri}/libro`, config)).json();
+    return res;
+}
+export const getOne = async(id)=>{
+    config.method = "GET";
+    // config.body = "";
+    let res = await (await fetch(`${uri}/libro/${id}`, config)).json();
+    return res;
+}
+export const getRelationships = async()=>{
+    config.method = "GET";
+    // config.body = "";
+    let res = await (await fetch(`${uri}/libro`, config)).json();
+    res = await Promise.all(res.map(async(data,id)=>{
+        let {categoriaId:catId, autorId:autId} = data;
+        let cat = await getOneCategoria(catId);
+        let aut = await getOneAutor(autId);
+        data.categoriaId = cat;
+        data.autorId = aut;
+        return data;
+    }))
+   
+    return res;
+}
+export const post = async(obj={})=>{
+    obj = validarExtructura(obj);
+    if(obj.status) return obj;
+    
+    config.method = "POST";
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}/libro`, config)).json();
+    return res;
+}
+export const deleteOne = async(id)=>{
+    if(typeof id !== 'number') return {status: 400, message: `El datos '${id}' no cumple con el formato`};
+    config.method = "DELETE";
+    // config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}/libro/${id}`, config)).json();
+    return res;
+}
+export const putOne = async(obj={})=>{
+    let all = undefined;
+    const {id, limit="1.0.0"} = obj;
+    if(typeof id !== 'number') return {status: 400, message: `El id '${id}' no cumple con el formato`};
+    if(limit == "1.0.0") {
+        obj = validarExtructura(obj);
+        if(obj.status) return obj;   
+    }
+    if(limit == "2.0.0") {
+        obj = validarDataBasic(obj);
+        if(obj.status) return obj;  
+        all = await getOne(id);
+    }
+    const {limit:lin, ...objUpdate} = obj;
+    obj = {...all, ...objUpdate};
+  
+    config.method = "PUT";
+    config.body = JSON.stringify(obj);
+    let res = await (await fetch(`${uri}/libro/${id}`, config)).json();
+    return res;
+}
 
 
-//console.log(await post({titulo:"pepito",fecha:"2023-0/-10"}));
-// console.log(await getAll());
-//console.log(await deleteOne(2));
+//console.log(await getRelationships());
 
-console.log(putOne({fechaLanzamiento:"24-08-2020"}));
+
+
+
+// console.log(await post({
+//     titulo:"El olor del miedo", 
+//     fecha: "2023-08-30", 
+//     autorId:1, autaautAuthenticatorAttestationResponse
+//     categoriaId:1, 
+//     editorialId:1, 
+//     isbn:"380554", 
+//     numPaginacion: 552, 
+//     estadoId:1
+// }));
+// console.log(await post({
+//     titulo:"FORASTERA", 
+//     fecha: "2006-08-30", 
+//     autorId:1, 
+//     categoriaId:2, 
+//     editorialId:1, 
+//     isbn:"9788418173745", 
+//     numPaginacion: 764, 
+//     estadoId:1
+// }));
+// console.log(await post({
+//     titulo:"Valle de la calma", 
+//     fecha: "2018-04-02", 
+//     autorId:1, 
+//     categoriaId:3, 
+//     editorialId:1, 
+//     isbn:"273059", 
+//     numPaginacion: 296, 
+//     estadoId:1
+// }));
+// console.log(await post({
+//     titulo:"Prometeo encadenado", 
+//     fecha: "2020-01-01", 
+//     autorId:1, 
+//     categoriaId:1, 
+//     editorialId:1, 
+//     isbn:"16758", 
+//     numPaginacion: 296, 
+//     estadoId:1
+// }));
+
+
+
+//console.log(await getAll());
+// console.log(await deleteOne(1));
+ //console.log(await putOne({id: 1, limit:"2.0.0", titulo: "Miguel", isbn: "456", numPaginacion:700}));
